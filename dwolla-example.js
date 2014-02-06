d0f368b3b151dc84729a239dde38b1ec400da8f5
Joseph Dykstra <josephdykstra@gmail.com>
2014-02-06 17:31:01 -0600

    Initial commit

---
+++ dwolla-example.js
@@ -0,0 +1,13 @@
+﻿var secret = require("../stripe-and-dwolla_sensitive-info/dwolla.js")
+
+var Main = require("./main.js")
+
+var whatever = function(err, data) {
+	if (err) {
+		console.log("Error: "+err+"\nTransaction ID: " + data);
+		return false
+	} else {
+		return true
+	}
+}
+
---
+++ dwolla-main.js
@@ -0,0 +1,11 @@
+﻿// Joseph Dykstra
+// 2014-02-06
+
+//Constructor
+module.exports = function(token, pin) {
+	var dwolla = require('dwolla')
+	
+	this.chargeCreate = function(destinationId, amount, func) {
+		dwolla.send( token, pin, destinationId, amount, func)
+	}
+}
